ba09bc41df00655f281da2025f8918ecc5b096a6
Hammad Mudassar <it.hammadmudassar5@gmail.com>
2025-07-25 12:04:40 +0500

    change the conflict comment them

--- LS_FA/main.py
+++ LS_FA/main.py
@@ -1,16 +1,16 @@
 from fastapi import FastAPI
 from .database import engine
 from . import models
-from .routers import ledger, stock
+# from .routers import ledger, stock
 
 # Create DB tables
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-# Include routers
-app.include_router(ledger.router, prefix="/ledger", tags=["Ledger"])
-app.include_router(stock.router, prefix="/stock", tags=["Stock"])
+# # Include routers
+# app.include_router(ledger.router, prefix="/ledger", tags=["Ledger"])
+# app.include_router(stock.router, prefix="/stock", tags=["Stock"])
 
 @app.get("/")
 def root():
